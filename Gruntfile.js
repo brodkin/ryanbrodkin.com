cceb2074bdf9e25ef6576c3958740abf4315d44d
Ryan Brodkin <ryan@brodkinca.com>
2013-03-12 18:17:42 -0700

    Fixing bug in which resume was not included in dist directory.

--- Gruntfile.js
+++ Gruntfile.js
@@ -226,7 +226,7 @@ module.exports = function (grunt) {
                     cwd: '<%= yeoman.app %>',
                     dest: '<%= yeoman.dist %>',
                     src: [
-                        '*.{ico,txt}',
+                        '*.{ico,txt,pdf}',
                         '.htaccess'
                     ]
                 }]
